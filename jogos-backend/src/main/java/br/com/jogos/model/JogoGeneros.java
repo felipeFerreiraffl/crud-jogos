40d979e86155180e70a346da9049bfc075ae1be6
felipeFerreiraffl <felipado2x0@gmail.com>
2024-12-08 16:47:58 -0300

    delete: deleted all files

--- jogos-backend/src/main/java/br/com/jogos/model/JogoGeneros.java
+++
@@ -1,5 +0,0 @@
-package br.com.jogos.model;
-
-public enum JogoGeneros {
-    ACAO, AVENTURA, TERROR, RPG, SIMULACAO, LUTA, CORRIDA, PLATAFORMA, MUSICA, ESTRATEGIA
-}
--- jogos-backend/src/main/java/br/com/jogos/repository/JogoRepository.java
+++
@@ -1,8 +0,0 @@
-package br.com.jogos.repository;
-
-import br.com.jogos.model.Jogo;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface JogoRepository extends JpaRepository<Jogo, Long> {}
