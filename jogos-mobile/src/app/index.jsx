7b331812a1750a91b92111d30e7619a31a693aca
felipeFerreiraffl <felipado2x0@gmail.com>
2024-12-13 20:28:45 -0300

    feat: created the first screen of mobile app

---
+++ jogos-mobile/src/app/index.jsx
@@ -0,0 +1,13 @@
+import { Text } from 'react-native';
+import * as S from './styles';
+
+export default function App() {
+  return (
+    <S.HomeView>
+      <S.HomeTitleContainer>
+        <Text>Olá</Text>
+        <Text>Image</Text>
+      </S.HomeTitleContainer>
+    </S.HomeView>
+  );
+}
---
+++ jogos-mobile/src/app/styles.js
@@ -0,0 +1,14 @@
+import styled from "styled-components/native";
+
+export const HomeView = styled.View`
+    flex: 1;
+    justify-content: center;
+    align-items: center;
+`;
+
+export const HomeTitleContainer = styled.View`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    gap: 5;
+`;
